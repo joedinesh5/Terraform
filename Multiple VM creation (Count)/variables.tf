423a9826fcdd5f3e0fa52eb620f1e47898b76da7
joedinesh5 <135306697+joedinesh5@users.noreply.github.com>
2024-09-04 13:44:01 +0530

    Create variables.tf

---
+++ Multiple VM creation (Count)/variables.tf
@@ -0,0 +1,39 @@
+variable "location" {
+    type = string
+    default = "Central India"
+  
+}
+variable "resgrp" {
+    type = string
+    default = "JoeSep"
+  
+}
+variable "subscriptionID" {
+    type = string
+    default = "d8620e47-e386-470c-bcb3-2b3c402a2198" 
+    
+}
+variable "TenantID" {
+    type = string
+    default = "11ce1e43-30f3-4fe7-898f-bd2457486c37"
+    
+  
+}
+
+variable "ClientId" {
+    type = string
+    default = "608f7031-62b4-4e3e-8406-9777fffbcc13"
+    
+  
+}
+variable "ClientSec" {
+    type = string
+    default = "UpT8Q~ylPT3V6ctYGk3Gb_e2qFo-S7R3s.OkEaVQ"
+    
+  
+}
+variable "vmname" {
+    type = string
+    default = "joevm"
+  
+}
